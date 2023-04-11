0fb7bfe6c40ccf9cbf70908d85f73c3c022fcc00
binon <binon_prabu@yahoo.com>
2023-04-11 10:55:07 +0100

    push, release

---
+++ Services/CookiePolicyService.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+
+namespace UK.NHS.CookieBanner.Services
+{
+    public interface ICookiePolicyService
+    {
+        /// <summary>
+        /// The LatestVersionAsync.
+        /// </summary>
+        /// <param name="id">Id.</param>
+        /// <returns>A <see cref="Task{TResult}"/> representing the result of the asynchronous operation.</returns>
+        Task<string> LatestVersionAsync(int id);
+    }
+    public class CookiePolicyService : ICookiePolicyService
+    {
+        public async Task<string> LatestVersionAsync(int id)
+        {
+            string viewmodel = string.Empty;
+
+            //var client = await GetClientAsync();
+
+            //var request = $"TermsAndConditions/LatestVersion/{tenantId}";
+            //var response = await client.GetAsync(request).ConfigureAwait(false);
+
+            //if (response.IsSuccessStatusCode)
+            //{
+            //    var result = response.Content.ReadAsStringAsync().Result;
+            //    viewmodel = JsonConvert.DeserializeObject<string>(result);
+            //}
+            //else if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized
+            //            ||
+            //         response.StatusCode == System.Net.HttpStatusCode.Forbidden)
+            //{
+            //    throw new Exception("AccessDenied");
+            //}
+
+            return viewmodel;
+        }
+    }
+}
---
+++ Services/GenericApiHttpClient.cs
@@ -0,0 +1,11 @@
+﻿namespace UK.NHS.CookieBanner.Services
+{
+    public interface IGenericApiHttpClient
+    {
+        Task<HttpClient> GetClientAsync();
+    }
+    public class GenericApiHttpClient : BaseHttpClient, IGenericApiHttpClient
+    {
+        public override string ApiUrl => "B";//  this.WebSettings.UserApiUrl;
+    }
+}
